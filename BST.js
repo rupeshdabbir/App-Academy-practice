a0cbefe48784682c8faf52a0f16649838f0eb440
rupeshdabbir <d.rupeshkumar@gmail.com>
2017-06-18 19:29:46 -0700

    BST: Implementation

--- BST.js
+++ BST.js
@@ -12,22 +12,56 @@ function Node(key){
     /* Basic Properties */
     this.left = null;
     this.right = null;
-    this.value = [];
+    //this.value = [];
     this.key = key;
+    this.deleted = false;
 
     /* Helper API's */
 
     this.insert = function(val) {
 
-        if(val == this.key) {
-
-        }
+        // if(val == this.key) {
+        //
+        // }
 
         if(val < this.key) {
-            this.left.insert(val);
+            if(this.left) {
+                this.left.insert(val);
+            } else {
+                this.left = new Node(val);
+            }
+
         } else {
-            this.right.insert(val);
+            if(this.right)
+                this.right.insert(val);
+            else
+                this.right = new Node(val);
         }
+    }
+
+    //Searching
+    this.lookup = function(val) {
+        if(this.key === val && !this.deleted){
+            return this; //this == node so we are returning the node.
+        } else if(val < this.key && this.left) {
+            this.left.lookup(val);
+        } else if(val > this.key && this.right) {
+            this.right.lookup(val);
+        }
+
+        return false; //if it doesn't exist in tree :-p
+    }
+
+    this.delete = function(val){
+        if(this.key == val) {
+            this.deleted = true;
+        } else if(val < this.key && this.left){
+            this.left.delete(val);
+        } else if(val > this.key && this.right) {
+            this.right.delete(val);
+        }
+
+        return false;
     }
 
 
