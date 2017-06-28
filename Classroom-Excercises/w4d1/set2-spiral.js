aa68e7a1662e0db7db3464a77277d92ab0b15480
rupeshdabbir <d.rupeshkumar@gmail.com>
2017-06-27 22:49:04 -0700

    Spiral Problem

---
+++ Classroom-Excercises/w4d1/set2-spiral.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(arr) {
+
+    let result = [];
+
+    if(!arr){
+        throw new Error("Array can't be empty");
+    }
+
+    while(true){
+
+        // console.log(arr[0].shift());
+        // console.log(arr)
+
+        if(arr.length === 0 || arr[0].length === 0)
+            break;
+
+        result = result.concat(arr.shift());
+
+
+        for(let i=0; i<arr.length; i++){
+            result.push(arr[i].pop());
+        }
+
+        if(arr.length === 0 || arr[0].length === 0)
+            break;
+
+        result= result.concat(arr.pop().reverse());
+
+        if(arr.length === 0 || arr[0].length === 0)
+            break;
+
+        for(let j=arr.length-1; j>-1; j--){
+            result.push(arr[j].shift());
+        }
+
+    }
+
+    return result;
+
+};
+
+var input = [
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ]
+];
+
+spiralOrder(input)
